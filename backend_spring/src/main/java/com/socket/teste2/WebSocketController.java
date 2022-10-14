d94ff305d05a90dc654be49e83c7bed61a770e72
Cristiano Lima <52179531+K16bits@users.noreply.github.com>
2022-10-14 17:15:35 -0300

    Update WebSocketController.java

--- backend_spring/src/main/java/com/socket/teste2/WebSocketController.java
+++ backend_spring/src/main/java/com/socket/teste2/WebSocketController.java
@@ -15,12 +15,23 @@ public class WebSocketController {
     @MessageMapping("/private-message")
     public Void privateMessage(@Payload Message message) throws InterruptedException{
         System.out.println("Messagem privata: "+message);
+
         Thread.sleep(1000);
-        simpMessagingTemplate.convertAndSendToUser(message.getNumeroPedido(),"/private", message); //
-        Thread.sleep(2000);
-        simpMessagingTemplate.convertAndSendToUser(message.getNumeroPedido(),"/private", message); // /user/nomeusuario/private
-        return null;
+        Map<String,String> data = new HashMap<>();
+        data.put("location", "123");
+        data.put("valor", "12.33");
+        data.put("status", "ATIVO");
+
+        String dataJSON_String = (new JSONObject(data)).toString();
+        simpMessagingTemplate.convertAndSendToUser(message.getNumeroPedido(),"/private",dataJSON_String); //
         
+        Thread.sleep(5000);
+        Map<String,String> data2 = new HashMap<>();
+        data2.put("location", "123");
+        data2.put("valor", "12.33");
+        data2.put("status", "CONCLUIDA");
+        simpMessagingTemplate.convertAndSendToUser(message.getNumeroPedido(),"/private", data2); // /user/nomeusuario/private
+        return null;
         // return message;
     }
 }
