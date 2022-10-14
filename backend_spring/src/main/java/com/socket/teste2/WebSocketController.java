67711afe24e10c72efcb9761c4e7b70e9f6fa4f3
Cristiano Lima <52179531+K16bits@users.noreply.github.com>
2022-10-14 18:59:53 -0300

    Update WebSocketController.java

--- backend_spring/src/main/java/com/socket/teste2/WebSocketController.java
+++ backend_spring/src/main/java/com/socket/teste2/WebSocketController.java
@@ -1,8 +1,14 @@
 package com.socket.teste2;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
@@ -13,7 +19,8 @@ public class WebSocketController {
     private SimpMessagingTemplate simpMessagingTemplate;
 
     @MessageMapping("/private-message")
-    public Void privateMessage(@Payload Message message) throws InterruptedException{
+    public Void privateMessage(@Payload Message message, @Header("simpSessionId") String sessionId) throws InterruptedException{
+        System.out.println(sessionId);
         System.out.println("Messagem privata: "+message);
 
         Thread.sleep(1000);
@@ -23,14 +30,14 @@ public class WebSocketController {
         data.put("status", "ATIVO");
 
         String dataJSON_String = (new JSONObject(data)).toString();
-        simpMessagingTemplate.convertAndSendToUser(message.getNumeroPedido(),"/private",dataJSON_String); //
+        simpMessagingTemplate.convertAndSendToUser(sessionId,"/private",dataJSON_String); //
         
         Thread.sleep(5000);
         Map<String,String> data2 = new HashMap<>();
         data2.put("location", "123");
         data2.put("valor", "12.33");
         data2.put("status", "CONCLUIDA");
-        simpMessagingTemplate.convertAndSendToUser(message.getNumeroPedido(),"/private", data2); // /user/nomeusuario/private
+        simpMessagingTemplate.convertAndSendToUser(sessionId,"/private", data2); // /user/nomeusuario/private
         return null;
         // return message;
     }
